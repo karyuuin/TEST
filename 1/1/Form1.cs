dcf5cc342631cdbd0593b6274bc4ba45c731773b
karyuuin <31936393+karyuuin@users.noreply.github.com>
2018-10-02 09:51:44 +0300

    Add files via upload

---
+++ 1/1/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x;
+            double s = 1.10;
+           x = (s - 1)/2;
+           richTextBox1.Text = x.ToString();
+        }
+    }
+}
